65ed709834c8acc362d8148a972dcac3b91f8106
David McGrann <dpmcgrann@gmail.com>
2018-01-21 17:57:29 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -11,9 +11,9 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board) == % 2
-    puts "X"
+  if turn_count(board)
+    return "X"
   else
-    puts "O"
+    return "O"
   end
 end
