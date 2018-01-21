1c446be90347521424608a488ca6a6d33b98abf8
David McGrann <dpmcgrann@gmail.com>
2018-01-21 17:08:51 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,10 @@
+def turn_count(board)
+  board = [ " ", " ", " ", " ", " ", " ", " ", " ", " " ]
+  board.each do |player|
+  end
+  counter = 0
+  until board == 9
+    counter += 1
+  end
+  return counter
+end
