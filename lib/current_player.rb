fef3062b70b48dc8cdaa0c2c24217e506e2bcb7f
David McGrann <dpmcgrann@gmail.com>
2018-01-21 17:22:44 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,12 @@
 def turn_count(board)
   board = [ " ", " ", " ", " ", " ", " ", " ", " ", " " ]
+  counter = 0
   board.each_with_index do |player|
     puts index
   end
+  if board == "X" || board == "O"
+    counter += 1
+    puts "#{counter}"
+  end
+  counter
 end
